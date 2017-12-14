62129cad064d7917b507609e6b8c5df2242be419
luzzif <fedeluzzi00@gmail.com>
2017-12-14 18:23:26 +0100

    Fix typing

---
+++ index.d.ts
@@ -0,0 +1,32 @@
+export { ApiVersion } from "./src/enums/ApiVersion";
+export { CandlestickInterval } from "./src/enums/CandlestickInterval";
+export { OrderExecutionStatus } from "./src/enums/OrderExecutionStatus";
+export { OrderRejectionMotive } from "./src/enums/OrderRejectionMotive";
+export { OrderSide } from "./src/enums/OrderSide";
+export { OrderStatus } from "./src/enums/OrderStatus";
+export { OrderType } from "./src/enums/OrderType";
+export { TimeInForce } from "./src/enums/TimeInForce";
+
+export { ApiError } from "./src/errors/ApiError";
+export { AuthenticationError } from "./src/errors/AuthenticationError";
+
+export { UserUpdate } from "./src/models/abstraction/UserUpdate";
+export { AccountData } from "./src/models/AccountData";
+export { AccountUpdate } from "./src/models/AccountUpdate";
+export { Balance } from "./src/models/Balance";
+export { CanceledOrderData } from "./src/models/CanceledOrderData";
+export { Candlestick } from "./src/models/Candlestick";
+export { CandlestickUpdate } from "./src/models/CandlestickUpdate";
+export { LatestPrice } from "./src/models/LatestPrice";
+export { Order } from "./src/models/Order";
+export { OrderBook } from "./src/models/OrderBook";
+export { OrderBookUpdate } from "./src/models/OrderBookUpdate";
+export { OrderUpdate } from "./src/models/OrderUpdate";
+export { PlacedOrder } from "./src/models/PlacedOrder";
+export { PlacedOrderData } from "./src/models/PlacedOrderData";
+export { Ticker } from "./src/models/Ticker";
+export { TickerStatistics } from "./src/models/TickerStatistics";
+export { Trade } from "./src/models/Trade";
+export { TradeUpdate } from "./src/models/TradeUpdate";
+
+export { BinanceApiClient } from "./src/BinanceApiClient";
